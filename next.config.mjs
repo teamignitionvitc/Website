a749dc6738176b4845e14c3db794c20dce7cd6cf
Anurag pradhan <134576365+anurag965@users.noreply.github.com>
2025-08-01 23:04:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,14 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    basePath: isProd ? '/Website' : '',
+    basePath: isProd ? '' : '',
     output: 'standalone',
     distDir: 'dist',
     images: {
         unoptimized: true,
     },
     env: {
-        NEXT_PUBLIC_BASE_PATH: isProd ? '/Website' : '',
+        NEXT_PUBLIC_BASE_PATH: isProd ? '' : '',
     },
 };
 
